152c159bcf858ba2a74f70ff1686844d2f8481ee
EdwardElric01 <1683260718@qq.com>
2018-05-01 20:17:07 +0800

    '2'

--- manga/entrypoint.py
+++ manga/entrypoint.py
@@ -5,7 +5,7 @@
  ---- Description:
  ---- Author: Ed
  ---- Date: 2018-04-29 23:15:25
- ---- Last modified: 2018-05-01 12:54:30
+ ---- Last modified: 2018-05-01 20:14:14
 """
 
 import mylibrary
@@ -19,11 +19,11 @@ if __name__ == '__main__':
     cur.execute('select homepage from project')
     homepages = [each[0] for each in cur.fetchall()]
     # skill: multiprocessing会出现不明原因的错误，出现什么copy,openssl之类的warning,可以尝试修改multiprocessing运行的函数名
-
     mylibrary.multirunning(insert.main, homepages)
+
     cur.execute('select pictureurl, picturename, chaptername, projectname from picture where ifdownload = 0')
     downloadlist = cur.fetchall()
+    cur.close()
     mylibrary.multirunning(download.main, downloadlist)
 
-    cur.close()
 
